e7169d098af0f0cab771c8149f7ce7f5b6b01823
SimonCos1 <simon.caussin@gmail.com>
2021-06-11 22:19:22 +0200

    Ajout fonction qui récupère chque url de livre pour une catégorie

--- books_scrap.py
+++ books_scrap.py
@@ -1,10 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import csv
 
 URL = "http://books.toscrape.com/"
-#BOOK_URL = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-BOOK_URL = "http://books.toscrape.com/catalogue/rip-it-up-and-start-again_986/index.html"
 CATEGORY = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html" #75 books
 
 def cleanning_title(title):
@@ -18,14 +16,14 @@ def generate_csv(book_data):
         writer = csv.DictWriter(f, fieldnames=labels)
         writer.writeheader()
         writer.writerow(book_data)
-    return print("CSV généré => OK \n")
 
 def generate_soup(url):
     get_url = requests.get(url)
     get_text = get_url.text
     return BeautifulSoup(get_text, "html.parser")
 
-def get_book_datas(BOOK_URL):
+"""
+def get_book_datas(books_urls_from_a_category):
     book_data = {}
     rubriques = {
     "UPC": "universal_ product_code (upc)",
@@ -33,48 +31,66 @@ def get_book_datas(BOOK_URL):
     "Price (incl. tax)": "price_including_tax", 
     "Availability": "number_available"
     }
-    soup = generate_soup(BOOK_URL)
-    # ciblages simples
-    book_data["product_page_url"] = BOOK_URL
-    book_data["title"] = cleanning_title(str(soup.title.text))
-    book_data["category"] = str(soup.findAll("a")[3].text)
-    book_data["image_url"] = soup.find("img").get("src").replace("../../", URL)
-    # on cible le 4ème paragraphe pour "product_description"
-    i = 0
-    for ps in soup.findAll("p"):
-        i += 1
-        if i == 4:
-            book_data["product_description"] = ps.text
-    
-    #review_rating : on récupère l'info dans l'attribut class du paragraphe
-    star_rating = soup.find("p", {"class" : "star-rating"})
-    book_data["review_rating"] = star_rating.get("class")[1]
-    # Récupération des infos du bloc "Product Information"
-    for trs in soup.findAll("tr"):
-        ths = trs.find("th").text
-        # si les titres th existent dans dict. rubriques, on enregistre les datas correspondantes.
-        if ths in rubriques.keys():
+    for book_url in books_urls_from_a_category:
+        soup = generate_soup(book_url)
+        # ciblages simples
+        book_data["product_page_url"] = book_url
+        book_data["title"] = cleanning_title(str(soup.title.text))
+        book_data["category"] = str(soup.findAll("a")[3].text)
+        book_data["image_url"] = soup.find("img").get("src").replace("../../", URL)
+        # on cible le 4ème paragraphe pour "product_description"
+        book_data["product_descritpion"] = str(soup.findAll("p")[3].text)
+        #review_rating : on récupère l'info dans l'attribut class du paragraphe
+        star_rating = soup.find("p", {"class" : "star-rating"})
+        book_data["review_rating"] = star_rating.get("class")[1]
+        # Récupération des infos du bloc "Product Information"
+        for trs in soup.findAll("tr"):
+            ths = trs.find("th").text
             book_data[rubriques.get(ths)] = trs.find("td").text
+            # si les titres th existent dans dict. rubriques, on enregistre les datas correspondantes.
+            if ths in rubriques.keys() and ths == "Price (excl. tax)" or "Price (incl. tax)":
+                book_data[rubriques.get(ths)] = trs.find("td").text[1:]  ####REGARDER BUG
+            else:
+                book_data[rubriques.get(ths)] = trs.find("td").text
+        
+    # affichage pour contrôle
+    for i in book_data.keys():
+        print(str(i) + " :  \n" + book_data.get(i) + "\n")
     
     generate_csv(book_data)
-    
-    def get_products_url(category):
-        """This function get each product's page url for a book category"""
-        url_prefix = "http://books.toscrape.com/catalogue/"
-        generate_soup(category)
-        #après <h3>, on clible <a href> et on récupère le lien
-        urls_category = []
+"""
+
+def get_products_urls(category):
+    # This function get each product's page url for a book category
+    books_urls_from_a_category = []
+    url_prefix = URL + "catalogue/"
+    i = 1
+    while category:
+        soup = generate_soup(category)
+        # après <h3>, on clible <a href> et on récupère le lien de la page du livre
         h3s = soup.findAll("h3")
         for h3 in h3s:
-            urls_category.append(h3.findNext(href=True).get("href").replace("../", url_prefix))
-        return print(urls_category)
+            books_urls_from_a_category.append(h3.findNext(href=True).get("href").replace("../../../", url_prefix))
 
-    #affichage pour contrôle    
-    """for i in book_data.keys():
-        print(i + " :  \n" + book_data.get(i) + "\n") """   
-    print("============================= \n")
-    get_products_url(CATEGORY)
+        # Gestion de la pagination pour les categories de plus d'une page de livres
+        if soup.findAll("a")[-1].text == "next":
+            next_btn = soup.select("a")[-1]['href']
+            print("next_btn : " + str(next_btn))
+            i += 1
+            print("category : " + str(category))
+            if next_btn == "page-2.html":
+                category = category.replace("index.html", f"page-{i}.html")
+            else:
+                category = category.replace(f"page-{i-1}.html", f"page-{i}.html")
+            print("category apr replace" + str(category))
+        else:
+            break
+    return books_urls_from_a_category
 
-    print("fin du test")
 
-get_book_datas(BOOK_URL)
+#get_book_datas(BOOK_URL)
+
+# affichage pour contrôle
+#print("=============category================ \n")
+
+print(get_products_urls(CATEGORY))
