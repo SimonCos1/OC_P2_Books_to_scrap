7a12b3e2f03f5338a0ef0faaa6f52cd8d6fab3a2
SimonCos1 <simon.caussin@gmail.com>
2021-06-17 22:46:21 +0200

    Améliorations (PEP8) et nettoyage du programme

--- books_scrap.py
+++ books_scrap.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 URL = "http://books.toscrape.com/"
 #CATEGORY = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html" #75 books
-CATEGORY = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html" #11 books, 1 page
+#CATEGORY = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html" #11 books, 1 page
 
 def cleanning_title(title):
     # This function is for cleanning the title for just keeping a string
@@ -21,14 +22,14 @@ def generate_csv(category, books_datas):
     # This function is for generating the CSV with 1 book datas
     labels = ["product_page_url", "title", "category", "image_url", "product_descritpion", "review_rating", "UPC", "Price (excl. tax)", 
             "Price (incl. tax)", "number_available"]
-    
     category = category.replace(" ", "_")
     with open(f"/home/simon/Documents/{category}.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(labels)
         for one_book_datas in books_datas:
             writer.writerow(one_book_datas)
-    print("CSV généré")
+    print(f"CSV {category} généré")
+    print("==============================")
 
 
 def get_products_urls(category):
@@ -49,7 +50,6 @@ def get_products_urls(category):
             next_btn = soup.select("a")[-1]['href']
             print("next_btn : " + str(next_btn))
             i += 1
-            print("category : " + str(category))
             if next_btn == "page-2.html":
                 category = category.replace("index.html", f"page-{i}.html")
             else:
@@ -67,22 +67,31 @@ def get_books_datas(category):
     values_list = []
     for book_url in books_urls_from_a_category:
         soup = generate_soup(book_url)
-        values_list.append(book_url) #product_page_url
-        values_list.append(cleanning_title(str(soup.title.text))) #title
-        values_list.append(str(soup.findAll("a")[3].text)) #category
-        values_list.append(soup.find("img").get("src").replace("../../", URL)) #image_url
-        values_list.append(str(soup.findAll("p")[3].text)) #product_descritpion
-        values_list.append(soup.find("p", {"class" : "star-rating"}).get("class")[1]) #review_rating : on récupère l'info dans l'attribut class du paragraphe
-        values_list.append(soup.findAll("td")[0].text) #UPC
-        values_list.append(soup.findAll("td")[2].text[1:]) #Price (excl. tax)
-        values_list.append(soup.findAll("td")[3].text[1:]) #Price (incl. tax)
-        values_list.append(soup.findAll("td")[5].text) #number_available
+        # product_page_url
+        values_list.append(book_url)
+        # title
+        values_list.append(cleanning_title(str(soup.title.text)))
+        # category
+        values_list.append(str(soup.findAll("a")[3].text))
+        # image_url
+        values_list.append(soup.find("img").get("src").replace("../../", URL))
+        # product_description
+        values_list.append(str(soup.findAll("p")[3].text))
+        #review_rating : on récupère l'info dans l'attribut class du paragraphe
+        values_list.append(soup.find("p", {"class" : "star-rating"}).get("class")[1])
+        # UPC
+        values_list.append(soup.findAll("td")[0].text)
+        # Price (excl. tax)
+        values_list.append(soup.findAll("td")[2].text[1:])
+        # Price (incl. tax)
+        values_list.append(soup.findAll("td")[3].text[1:])
+        # number_available
+        values_list.append(soup.findAll("td")[5].text)
         books_datas.append(values_list)
         values_list = []
+
     category = str(soup.findAll("a")[3].text)
-
-    generate_csv(category, books_datas) ### GENERATE A DEPLACER DANS PROCHAINE FONCTION
-
+    generate_csv(category, books_datas)
     return(category, books_datas)
 
 
