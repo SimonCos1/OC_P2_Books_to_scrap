4e16430e45966453a739e41322ab64108dbf4e4a
SimonCos1 <simon.caussin@gmail.com>
2021-06-19 10:37:02 +0200

    Ajout de la fonction create_folder qui prend en charge la création de répertoires

--- books_scrap.py
+++ books_scrap.py
@@ -19,15 +19,17 @@ def generate_soup(url):
 
 
 def generate_csv(category, books_datas):
-    # This function is for generating the CSV with 1 book datas
+    # This function is for generating the CSV with book datas.
     labels = ["product_page_url", "title", "category", "image_url", "product_descritpion", "review_rating", "UPC", "Price (excl. tax)", 
             "Price (incl. tax)", "number_available"]
     category = category.replace(" ", "_")
-    with open(f"/home/simon/Documents/{category}.csv", "w", newline="") as f:
+    folder_path = create_folder("CSV_extracted")
+    with open(os.path.join(folder_path, f"{category}.csv"), "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(labels)
         for one_book_datas in books_datas:
             writer.writerow(one_book_datas)
+
     print(f"CSV {category} généré")
     print("==============================")
 
@@ -56,12 +58,12 @@ def get_products_urls(category):
                 category = category.replace(f"page-{i-1}.html", f"page-{i}.html")
         else:
             break
-    print("Nombre de livres traités : " + str(len(books_urls_from_a_category)))
+    print("Nombre de catégories de livres trouvées : " + str(len(books_urls_from_a_category)))
     return books_urls_from_a_category
 
 
 def get_books_datas(category):
-    # Extrait les données de chaque livre puis génération d'un csv
+    # Extract datas for each book page.
     books_urls_from_a_category = get_products_urls(category)
     books_datas = []
     values_list = []
@@ -88,25 +90,34 @@ def get_books_datas(category):
         # number_available
         values_list.append(soup.findAll("td")[5].text)
         books_datas.append(values_list)
-        values_list = []
+        values_list.clear()
 
     category = str(soup.findAll("a")[3].text)
-    generate_csv(category, books_datas)
     return(category, books_datas)
 
 
+def create_folder(repository):
+    # This function create needed repository (on the current folder) and return the folder's path
+    current_folder = os.path.dirname(__file__)
+    new_folder = os.path.join(current_folder, repository)
+    os.makedirs(new_folder, exist_ok=True)
+    return new_folder
+    
+
 def complete_extract(URL):
-    # extract categories' URLs
+    # main function - Scrap the website
     categories_urls = []
     soup = generate_soup(URL).find("div", {"class": "side_categories"})
     categories_partial_links = soup.findAll("a", href=True)
     for a in categories_partial_links:
         categories_urls.append(URL + str(a.get("href")))
+    # The first list's item isn't a valid category
     categories_urls = categories_urls[1:]
     print("Nombre de catégories traitées : " + str(len(categories_urls)))
     
     for category in categories_urls:
-        get_books_datas(category)
+        one_category_of_books, books_datas = get_books_datas(category)
+        generate_csv(one_category_of_books, books_datas)
 
 
 complete_extract(URL)
